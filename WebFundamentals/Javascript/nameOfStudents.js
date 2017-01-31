0ff37ae4b74ae3b4e4bec4612068186c455faec1
Victor Adu <aduvictor@gmail.com>
2017-01-31 12:11:41 -0800

    Fixed JS Scroll Bugs

---
+++ WebFundamentals/Javascript/nameOfStudents.js
@@ -0,0 +1,10 @@
+var students = [
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
+
+for (var student in students){
+ console.log(students[student].first_name + " " + students[student].last_name);
+}
