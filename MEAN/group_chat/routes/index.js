901f751c129a78e3702e47afe5e734f805bbea37
Victor Adu <aduvictor@gmail.com>
2017-03-13 10:55:05 -0700

    Get group chat web app working

--- MEAN/group_chat/routes/index.js
+++ MEAN/group_chat/routes/index.js
@@ -1,21 +1,39 @@
-module.exports = function Route(app, server){
-	var io = require('socket.io').listen(server)
+var users = [];
 
-	io.sockets.on('connection', function (socket) {
-		socket.on("push_button", function (data){
-      console.log(data);
+var messages = [];
+
+var is_user = function(user) {
+  var users_count = users.length;
+
+  for (var i = 0; i < users_count; i++) {
+    if (user == users[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
-		})
-		// //resetting the counter into 0
-		// socket.on("reset_counter", function (data){
-		// 	counter = 0;
-		// 	io.emit('reset_response', {response: counter});
-		// })
-	})
+module.exports = function Route(app, server) {
+  var io = require("socket.io").listen(server)
 
-	// root route to render the index.ejs view
-	app.get('/', function(req, res) {
-	 res.render("index");
-	})
-};
+  io.sockets.on("connection", function(socket){
+    socket.on("page_load", function(data){
+      if(is_user(data.user) === true) {
+        socket.emit("existing_user", {error: "This user already exits"})
+      } else {
+        users.push(data.user);
+        socket.emit("load_messages", {current_user: data.user, messages: messages})
+      }
+    })
+
+    socket.on("new_message", function(data){
+      messages.push({name: data.user, message: data.message})
+      io.emit("post_new_message", {new_message: data.message, user: data.user})
+    })
+  })
+
+  app.get("/", function(request, response){
+    response.render("index")
+  })
+}
